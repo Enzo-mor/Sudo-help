a94cbf9b8b22d18aff3e2a8bce153faa49a9b6c2
Enzo <enzo.moreau.rtu@univ-lemans.fr>
2025-02-28 14:07:35 +0100

    modifiation interface

--- src/main/java/com/grp6/InterfaceTech.java
+++ src/main/java/com/grp6/InterfaceTech.java
@@ -3,4 +3,7 @@ package com.grp6;
 public interface InterfaceTech {
     
     public boolean detect(Grid grille);
+
+    //Applique la technique dans une copie de la grille du joueur
+    public void applique(Grid grilleJoueur);
 }
