bc39c699b4d7c7a36bce79381a0b7aafdbf285f7
dgrammont <dylangrammont3@gmail.com>
2025-03-19 13:46:35 +0100

    Create LastNumber.java

---
+++ src/main/java/grp6/syshelp/LastNumber.java
@@ -0,0 +1,17 @@
+package grp6.syshelp;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import grp6.sudocore.Cell;
+import grp6.sudocore.Grid;
+
+public class LastNumber implements InterfaceTech {
+    
+    @Override
+    public Help getgHelp(Grid grille){
+
+        
+    }
+}
