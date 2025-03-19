bbdb08c57c44ead8e1a1c2380e2813251c102d8f
Roquain Louison <s2202705@ic2s122-15.univ-lemans.fr>
2025-03-19 16:19:26 +0100

    maj LastNumber & LastPossible

--- src/main/java/grp6/syshelp/LastPossible.java
+++ src/main/java/grp6/syshelp/LastPossible.java
@@ -12,7 +12,7 @@ public class LastPossible implements InterfaceTech{
     
     
     public Help getHelp(Grid grille){
-        for(int j=0; j<9; j++){
+        for(int j=0; j<Grid.NB_NUM; j++){
             int[] num =grille.numToPosForSubGrid(j);
             Cell[] car_cells=grille.getFlatSubGrid(num[0],num[1]);
             List<Cell> cellules = new ArrayList<>();
@@ -31,18 +31,43 @@ public class LastPossible implements InterfaceTech{
 
             annoteSolo.stream()
                 .forEach(b -> IntStream.range(0, b.length)
-                    .filter(i -> b[i])
-                    .forEach(i -> occurrences[i]++)
+                    .filter(ind -> b[ind])
+                    .forEach(ind -> occurrences[ind]++)
                 );
             
-            for(Integer i: occurrences)
-                if(i==SOLO){
+            for(int i=0;i<Grid.NB_NUM;i++)
+                if(occurrences[i]==SOLO){
                     Help h =new Help(this.getClass().getSimpleName());
                     h.addSquare(j%3,j/3);
+
+                    aide.setMessage(1,"Fait attention au "+i);
+                    aide.setMessage(2,"Fait attention aux Carré (3*3) "+leCarre(j));
+                    aide.setMessage(3,"Regarde LA"+);
                     return h;
                 }
         }
         return null;
+    }
+
+
+    /**
+     * Retourne la phrase correspondant à la zone de 3×3 cellules.
+     * @param indCarre la position du carré : [[1,2,3], [4,5,6], [7,8,9]]
+     * @return La phrase correspondante de la zone 3×3.
+     */
+    private String leCarre(int indCarre) {
+        switch (indCarre) {
+            case 1: return " en haut à gauche";
+            case 2: return " en haut au centre";
+            case 3: return " en haut à droite";
+            case 4: return " au milieu à gauche";
+            case 5: return " au milieu au centre";
+            case 6: return " au milieu à droite";
+            case 7: return " en bas à gauche";
+            case 8: return " en bas au centre";
+            case 9: return " en bas à droite";
+            default: return "Indice invalide, doit être entre 1 et 9";
+        }
     }
 
     public static void main(String[] args) {
