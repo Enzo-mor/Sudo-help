a55e31fc9e787793961ea701b28247b057da3dd1
Roquain Louison <s2202705@ic2s122-15.univ-lemans.fr>
2025-03-19 15:45:35 +0100

    maj LastNumber

--- src/main/java/grp6/syshelp/LastNumber.java
+++ src/main/java/grp6/syshelp/LastNumber.java
@@ -11,9 +11,7 @@ public class LastNumber implements InterfaceTech {
     
     @Override
     public Help getHelp(Grid grille) {
-        Help aideColl = new Help(getClass().getSimpleName());
-        Help aideLine = new Help(getClass().getSimpleName());
-        Help aideSquare = new Help(getClass().getSimpleName());
+        Help aide = new Help(getClass().getSimpleName());
 
         
         ArrayList<Integer> listeColl = new ArrayList<>();
@@ -21,71 +19,16 @@ public class LastNumber implements InterfaceTech {
         ArrayList<Integer> listeSquare = new ArrayList<>();
 
         int[]  d;
-
+        AutoAnnotation.generate(grille);
         for(int i=0; i<Grid.NB_NUM; i++){
-            for(int j=0;j<Grid.NB_NUM;j++){
-                if(grille.getCell(i, j).isEmpty()){
-                    
-                    List<Cell> col_cells=grille.fullCell(Grid.Shape.COLUMN,j);
-                    List<Cell> lin_cells=grille.fullCell(Grid.Shape.LINE,i);
-                    List<Cell> car_cells=grille.fullCell(Grid.Shape.SQUARE,i);
+            for(int j=0; j<Grid.NB_NUM; j++){      
+                if(grille.getCell(i,j).getAnnotations().size()==1){
+                    aide.addPos(i,j)
+                    aide.addLine(i);
 
-                    for(Cell c:col_cells){
-
-                
-                        if(!listeColl.contains(c.getNumber()) ){
-                            listeColl.add(c.getNumber());
-
-                            d = c.getPosition();
-                            aideColl.addPos(d[0], d[1]);
-                            aideColl.addColumn(i);
-                            aideColl.setMessage(1, "Fait attention au"+findLastNumber(grille.getColumn(i)));
-                            aideColl.setMessage(2, "Fait attention aux colonnes");
-                            aideColl.setMessage(3, "Regarde ici");
-
-                        }
-
-                    }
-
-                    for(Cell c:lin_cells){
-
-                        if(!listeLine.contains(c.getNumber()) ){
-                            listeLine.add(c.getNumber());
-
-                            d = c.getPosition();
-                            aideLine.addPos(d[0], d[1]);
-                            aideLine.addLine(i);
-                            aideLine.setMessage(1, "Fait attention au"+findLastNumber(grille.getLine(i)));
-                            aideLine.setMessage(2, "Fait attention aux lignes");
-                            aideLine.setMessage(3, "Regarde ici");
-                        }
-                    }
-
-                    for(Cell c:car_cells){
-
-                        if(!listeSquare.contains(c.getNumber()) ){
-                            listeSquare.add(c.getNumber());
-                            int[] temp = grille.numToPosForSubGrid(i);
-                            d = c.getPosition();
-                            aideSquare.addPos(d[0], d[1]);
-                            aideSquare.addSquare(i);
-                            aideSquare.setMessage(1, "Fait attention au"+grille.getFlatSubGrid(temp[0],temp[1]));
-                            aideSquare.setMessage(2, "Fait attention aux carrés");
-                            aideSquare.setMessage(3, "Regarde ici");
-
-                        }
-                    }
-        
-                    if(listeColl.size() == Grid.NB_NUM-1){
-                        return aideColl;
-                    }
-                    if (listeLine.size() == Grid.NB_NUM-1){
-                        return aideLine;
-                    }
-                    if (listeSquare.size() == Grid.NB_NUM-1){
-                        return aideSquare;
-                    }
-                    
+                    aide.setMessage(1,"Fait attention au "+grille.getCell(i,j).getAnnotations()[0]);
+                    aide.setMessage(2,"Fait attention aux ligne "+i);
+                    aide.setMessage(3,"Regarde la cellule ligne :  "+ i +", colone : "+ j+" Bissous" );
                 }
             }
         }
